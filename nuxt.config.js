830145b48e0efaa8bbb2d1dc8366e07326a61717
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-06-23 00:52:53 -0400

    first query ~

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 import { defineNuxtConfig } from "nuxt";
 
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/sanity"],
+  sanity: {
+    projectId: "3hmcprbp",
+		apiVersion: '2022-03-25'
+  },
   css: ["@/assets/css/styles.css"]
 });
--- pages/index.vue
+++ pages/index.vue
@@ -27,16 +27,18 @@
 </template>
 
 <script setup>
-const { data } = {
+const query = groq`*[_type == "animal"]`;
+const { data } = useSanityQuery(query);
+/* {
   data: [
-    /* {
+     {
       _createdAt: "2022-03-08T09:28:00Z",
       _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
       _rev: "xnBg0xhUDzo561jnWODd5e",
       _type: "pet",
       _updatedAt: "2022-03-08T09:28:00Z",
       name: "Bamse",
-    }, */
-  ],
-};
+    }, 
+    ],
+}; */
 </script>
